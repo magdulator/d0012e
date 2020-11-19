fd1f99ef7f92cb6e54fa0ff8c51ebce862172e13
Marcus <Marcus99.ma@gmail.com>
2020-11-19 17:56:39 +0100

    Fixed comments

--- d0012e/heap.py
+++ d0012e/heap.py
@@ -1,12 +1,14 @@
 class heap:
-    #Heap constructor
+    ################################################################
+    # Heap constructor
     def __init__(self, capacity):
         self.heap = []            #Heap List
         self.currentSize = 0      #Current size of heap list
         self.capacity = capacity  #Max Capacity of the heap list
-    
 
-    #Inserts input at the last position
+    
+    ################################################################
+    # Inserts input at the last position
     # O(c) cost
     def insert(self, input):
         if self.isFull():
@@ -16,7 +18,9 @@ class heap:
             self.fixHeap(self.currentSize)
             self.currentSize += 1
 
-    #sorts the list 
+
+    ################################################################
+    # sorts the list 
     # O(n)
     def fixHeap(self, index):
         newValue = self.heap[index]
@@ -28,13 +32,16 @@ class heap:
         self.heap[index] = newValue
 
 
-    #Checks if heap is full returns true if true
+    ################################################################
+    # Checks if heap is full returns true if true
     # O(c) cost
     def isFull(self): 
         return self.currentSize == self.capacity
     
-    #Retuns parent's index
-    #O(c)
+
+    ################################################################
+    # Retuns parent's index
+    # O(c)
     def getParent(self, index):
         return (index-1)/2
     
