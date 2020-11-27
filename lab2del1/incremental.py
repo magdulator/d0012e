90df5e9cfcf4164e8af92cc93929d70d299f6ba6
magda <magdaa.a@hotmail.com>
2020-11-27 12:36:16 +0100

    counting sort works

--- lab2del1/incremental.py
+++ lab2del1/incremental.py
@@ -8,8 +8,24 @@ def generateList(maxNum, length):
     return randomList
 
 def incrementalAlgo(randomList):
-    #sort by some algorithm
     print(randomList)
+    countArray = [0]*(max(randomList)+1) 
+    sortedArray = [0]*(len(randomList)+1) 
+
+    for i in range(0, len(randomList)):
+        countArray[randomList[i]] += 1
+    print("Occurrencies for each index:            ", countArray)
+
+    for i in range(0, len(countArray)-1):
+        countArray[i+1] = countArray[i] + countArray[i + 1]
+    print("Add occurrency with previous occurency: ", countArray)
+
+    for i in range(0, len(randomList)):
+        putInList = randomList[i]
+        sortedArray[countArray[putInList]] = putInList
+        countArray[putInList] -= 1
+    del(sortedArray[0])
+    print(sortedArray)
 
 def main():
     random = generateList(10, 10)
