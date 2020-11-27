bd9af1a05aefed2cf45feaf7ded2c66fa85748df
magda <magdaa.a@hotmail.com>
2020-11-27 11:41:42 +0100

    generate random list

--- lab2del1/incremental.py
+++ lab2del1/incremental.py
@@ -1,1 +1,18 @@
-#hej
+import random
+
+def generateList(maxNum, length):
+    randomList = []
+    for i in range(0, length):
+        n = random.randint(0,maxNum)
+        randomList.append(n)   
+    return randomList
+
+def incrementalAlgo(randomList):
+    #sort by some algorithm
+    print(randomList)
+
+def main():
+    random = generateList(10, 10)
+    incrementalAlgo(random)
+
+main()
