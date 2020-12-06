6ba422bee216cc9e48a800e0caf1e9ac0b83ac2c
Cordobro <60513969+Cordobro@users.noreply.github.com>
2020-12-06 11:56:05 +0100

    Update DACFind3Smallest.py

--- lab2del1/DACFind3Smallest.py
+++ lab2del1/DACFind3Smallest.py
@@ -37,8 +37,15 @@ def divideAndFindSmallest(randomList):
             return divideAndFindSmallest(randomList)
 
 
-
-
-
-
 main()
+
+
+# T(3) = 3 , (*)
+# T(n) = 3 + T(n-1), when n>3. (**)
+#
+# T(n) = (**)
+# 3 + T(n-1) = (**)
+# 3 + ( 3 + T(n-2)) = 6 + T(n-2) = (**)
+# 3k + T(n-k) = .. = 3(n-3) + T(3) = 3n -9
+#  
+# ==> O(n)
