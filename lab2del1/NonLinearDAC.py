0851aca0730929cc2ef6ebe42d7a03207b7059ca
magda <magdaa.a@hotmail.com>
2020-12-08 11:51:08 +0100

    fixade basfallet

--- lab2del1/NonLinearDAC.py
+++ lab2del1/NonLinearDAC.py
@@ -13,7 +13,10 @@ def generateList(maxNum, length):
 def DivideAndConquerQ(arr):
     #print(arr)
     if len(arr) == 2:
-        return arr[1]/arr[0]
+        if arr[1] > arr[0]:
+            return arr[1]/arr[0]
+        else:
+            return arr[0]/arr[1]
 
     mid=int(len(arr)/2)     # Cut the array into two 
     left  = arr[0:mid]
@@ -39,7 +42,7 @@ def test():
         i *= 2
         
 def main():
-    random = generateList(10, 16)
+    random = generateList(20, 2)
     print(DivideAndConquerQ(random))
     print(random)
     
